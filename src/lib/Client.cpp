dac38b4c4807fe176f8df8d5568231a0c2e0ff05
NamPE286 <nambuihung654@gmail.com>
2024-04-22 00:04:08 +0700

    update

--- src/lib/Client.cpp
+++ src/lib/Client.cpp
@@ -5,11 +5,10 @@ void Client::submitAttempt(size_t count) {
 	using namespace geode::prelude;
 	
 	auto APIKey = Mod::get()->getSettingValue<std::string>("API key");
-	auto APIUrl = Mod::get()->getSettingValue<std::string>("API url");
-	std::string url = APIUrl + std::string("/player/heatmap/") + std::to_string(count);
-	std::string curlCmd = "curl.exe -X POST \"" + url + "\" --header \"Authorization: Bearer " + APIKey + "\"";
-	log::info("{}", APIKey);
-	log::info("{}", curlCmd);
-	log::info("{}", "Sending data to " + url);
-	system(curlCmd.c_str());
+	auto APIUrl = "https://dlvnapi-nampe.koyeb.app";
+	std::string urlPath = "/player/heatmap/" + std::to_string(count);
+	
+	web::AsyncWebRequest()
+		.header("Authorization", "Bearer " + APIKey)
+		.post(APIUrl + urlPath);
 }
